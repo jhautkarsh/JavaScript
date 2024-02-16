dcd6b617886ef542ac4079b1bba884db80a14a7a
utkarsh jha <113284417+jhautkarsh@users.noreply.github.com>
2024-02-16 14:08:17 +0000

    day1

--- 01_basics/Day1.js
+++ 01_basics/Day1.js
@@ -33,3 +33,32 @@ console.log(typeof name)       // String
 console.log(typeof age)        // Integer
 console.log(typeof undefined)  // undefined
 console.log(typeof null)       // Object
+
+
+// Lectures 4 : Conversion
+
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+// "33"   => 33
+// "33abc"=> NaN
+// true => 1 ||  false => 0
+
+
+let isloggedIn2 = 1
+let BooleanIsLoggedIn = Boolean(isloggedIn2);
+console.log(BooleanIsLoggedIn); // true
+
+//  0 => false || 1 => true
+// "" => false
+// "name" = true
+
+let num = 33;
+let numToStr = String(num)
+console.log(typeof numToStr); // String
